bb7e62cc52569c12b264f7e5c5dee8bbbfcc983b
aravindh1208 <72460235+aravindh1208@users.noreply.github.com>
2021-04-11 01:11:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+
 const app = express();
 
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/index.html");
 })
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("server started");
 });
